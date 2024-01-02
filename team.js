79f9dea6d84075372ce0fc21432c3a24ce0b1d47
Tega <tegaizu98@gmail.com>
2024-01-02 02:40:00 +0100

    Aggiornato con info-Tega

--- team.js
+++ team.js
@@ -1,1 +1,15 @@
-let arr = [];
+let arr = [
+{
+name : "Oghenetega",
+surname : "Izu",
+age : 25,
+city : "Roma",
+hobby : "Musica",
+favoriteFood : "Carbonara",
+favoriteVideoGame : "fifa",
+favoriteFilm : "Lion King",
+favoriteBook : "Rich dad Poor dad",
+petName : "Tjay"
+}
+
+];
