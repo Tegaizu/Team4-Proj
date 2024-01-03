6f720126b6fa450a84900bdd45ea16e9f40986b4
RiccardoSideri <155484935+RiccardoSideri@users.noreply.github.com>
2024-01-04 00:55:02 +0100

    Update team.js

--- team.js
+++ team.js
@@ -1,15 +1,45 @@
-let arr = [
-{
-name : "Oghenetega",
-surname : "Izu",
-age : 25,
-city : "Roma",
-hobby : "Musica",
-favoriteFood : "Carbonara",
-favoriteVideoGame : "fifa",
-favoriteFilm : "Lion King",
-favoriteBook : "Rich dad Poor dad",
-petName : "Tjay"
-}
+let anotherPerson = {
+  name: "Riccardo",
+  surname: "Sideri",
+  age: 24,
+  city: "Carbonia",
+  hobby: "GinTonic",
+  favoriteFood: "Sushi",
+  favoriteVideoGame: "LeagueOfLegend",
+  favoriteFilm: "IoSonoLeggenda",
+  favoriteBook: "Berserk",
+  petName: "Micio"
+};
 
-];
+
+peopleArray.push(anotherPerson);
+
+
+console.log(peopleArray);
+
+
+
+function printPeopleWithPets(peopleArray) {
+    console.log("People with pets:");
+    for (let person of peopleArray) {
+      if (person.petName) {
+        console.log(`${person.name} has a pet named ${person.petName}`);
+      }
+    }
+  }
+  
+  
+  function printPeopleWithFavoriteGame(peopleArray) {
+    console.log("People with 'LOL' or 'League Of Legends' as favorite video game:");
+    for (let person of peopleArray) {
+      const favoriteGame = person.favoriteVideoGame.toLowerCase();
+      if (favoriteGame === 'lol' || favoriteGame === 'league of legends') {
+        console.log(`${person.name} likes 'LOL' or 'League Of Legends'`);
+      }
+    }
+  }
+  
+
+  printPeopleWithPets(peopleArray);
+  printPeopleWithFavoriteGame(peopleArray);
+  
